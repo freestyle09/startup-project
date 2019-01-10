d791c00256277cc99991bf9e790f0a02aa843fdb
freestyle09 <jozefrzadkosz09@gmail.com>
2019-01-10 15:54:09 +0100

    changed startup configuration

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import '../styles/style.scss';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className='container'>
+        <i className='fa fa-heart' /> App Component! :)
+      </div>
+    );
+  }
+}
+
+export default App;
